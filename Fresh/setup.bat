6ef26b87a3a56a58cc1779b81c7bf57b5a6c2cc7
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-01-04 17:38:44 +0300

    Add files via upload

---
+++ Fresh/setup.bat
@@ -0,0 +1,21 @@
+@echo off
+pushd "%~dp0"
+
+echo :: Checking For Administrator Elevation...
+timeout /t 1 /nobreak > NUL
+
+openfiles > NUL 2>&1
+if %errorlevel%==0 (
+        echo.
+        echo :: Elevation found! Proceeding...
+) else (
+        echo :: You are NOT running as Administrator!
+        echo.
+        echo  Right-click and select ^'Run as Administrator^' and try again.
+        echo.
+        echo  Hit RETURN to exit...
+        pause > NUL
+        exit 1
+)
+PowerShell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force | .\Fresh.ps1"
+pause > NUL
