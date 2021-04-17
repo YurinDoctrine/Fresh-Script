27ac614e87f467641a44ae06e415d6ba07ea52c5
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-04-17 12:02:30 +0300

    Add files via upload

--- Fresh/Fresh.psm1
+++ Fresh/Fresh.psm1
@@ -1429,125 +1429,6 @@ function OOShutup {
 	./OOSU10.exe ooshutup.cfg /quiet
 }
 #endregion O&OShutup
-#region UWP apps
-<#
-	Uninstall UWP apps
-	A dialog box that enables the user to select packages to remove
-	App packages will not be installed for new users if "Uninstall for All Users" is checked
-	Add UWP apps packages names to the $UncheckedAppXPackages array list by retrieving their packages names using the following command:
-		(Get-AppxPackage -PackageTypeFilter Bundle -AllUsers).Name
-
-	Удалить UWP-приложения
-	Диалоговое окно, позволяющее пользователю отметить пакеты на удаление
-	Приложения не будут установлены для новых пользователе, если отмечено "Удалять для всех пользователей"
-	Добавьте имена пакетов UWP-приложений в массив $UncheckedAppXPackages, получив названия их пакетов с помощью команды:
-		(Get-AppxPackage -PackageTypeFilter Bundle -AllUsers).Name
-#>
-function UninstallUWPApps {
-	# UWP apps that won't be shown in the form
-	# UWP-приложения, которые не будут выводиться в форме
-	$ExcludedAppxPackages = @(
-
-		# Microsoft Store
-		"Microsoft.WindowsStore",
-		
-		# AMD Radeon UWP panel
-		# UWP-панель AMD Radeon
-		"AdvancedMicroDevicesInc*",
-
-		# NVIDIA Control Panel
-		# Панель управления NVidia
-		"NVIDIACorp.NVIDIAControlPanel",
-
-		# Realtek Audio Control
-		"RealtekSemiconductorCorp.RealtekAudioControl"
-		
-	)
-	
-	if (Get-AppxPackage -PackageTypeFilter Bundle -AllUsers | Where-Object -FilterScript { $_.Name -cnotmatch ($ExcludedAppxPackages -join "|") } | Remove-AppxPackage -AllUsers ) {
-		Write-Verbose -Message 'Removed UWP apps' -Verbose
-	}
-	else {
-		Write-Verbose -Message $Localization.NoData -Verbose
-	}
-}
-
-# Do not let UWP apps run in the background, except the followings... (current user only)
-# Не разрешать UWP-приложениям работать в фоновом режиме, кроме следующих... (только для текущего пользователя)
-function DisableBackgroundUWPApps {
-	New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications -Name GlobalUserDisabled -PropertyType DWord -Value 1 -Force
-	Get-ChildItem -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications | ForEach-Object -Process {
-		Remove-ItemProperty -Path $_.PsPath -Name * -Force
-	}
-
-	$ExcludedBackgroundApps = @(
-
-		# Windows Search
-		"Microsoft.Windows.Search",
-
-		# Windows Security
-		# Безопасность Windows
-		"Microsoft.Windows.SecHealthUI",
-
-		# Microsoft Store
-		"Microsoft.WindowsStore"
-	)
-	$OFS = "|"
-	Get-ChildItem -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications | Where-Object -FilterScript { $_.PSChildName -notmatch "^$($ExcludedBackgroundApps.ForEach({[regex]::Escape($_)}))" } | ForEach-Object -Process {
-		New-ItemProperty -Path $_.PsPath -Name Disabled -PropertyType DWord -Value 1 -Force
-		New-ItemProperty -Path $_.PsPath -Name DisabledByUser -PropertyType DWord -Value 1 -Force
-	}
-	$OFS = " "
-}
-
-# Disable the following Windows features
-# Отключить следующие компоненты Windows
-function DisableWindowsFeatures {
-	$WindowsOptionalFeatures = @(
-		# Media Features
-		# Компоненты работы с мультимедиа
-		"MediaPlayback",
-
-		# Work Folders Client
-		# Клиент рабочих папок
-		"WorkFolders-Client"
-	)
-	Disable-WindowsOptionalFeature -Online -FeatureName $WindowsOptionalFeatures -NoRestart
-}
-
-# Disable certain Feature On Demand v2 (FODv2) capabilities
-# Отключить определенные компоненты "Функции по требованию" (FODv2)
-function DisableWindowsCapabilities {
-	# The following FODv2 items will be shown, but their checkboxes would be clear
-	# Следующие дополнительные компоненты будут видны, но их чекбоксы не будут отмечены
-	$ExcludedCapabilities = @(
-		# The DirectX Database to configure and optimize apps when multiple Graphics Adapters are present
-		# База данных DirectX для настройки и оптимизации приложений при наличии нескольких графических адаптеров
-		"DirectX.Configuration.Database*",
-
-		# Language components
-		"Language.*"
-	)
-	
-	if (Get-WindowsCapability -Online | Where-Object -FilterScript { ($_.State -eq "Installed") -and ($_.Name -cnotmatch ($ExcludedCapabilities -join "|")) } | Remove-WindowsCapability -Online ) {
-		Write-Verbose -Message 'Removed Capabilities' -Verbose
-	}
-	else {
-		Write-Verbose -Message $Localization.NoData -Verbose
-	}
-}
-
-# Turn off Cortana autostarting
-# Удалить Кортана из автозагрузки
-function DisableCortanaAutostart {
-	if (Get-AppxPackage -Name Microsoft.549981C3F5F10) {
-		if (-not (Test-Path -Path "Registry::HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\SystemAppData\Microsoft.549981C3F5F10_8wekyb3d8bbwe\CortanaStartupId")) {
-			New-Item -Path "Registry::HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\SystemAppData\Microsoft.549981C3F5F10_8wekyb3d8bbwe\CortanaStartupId" -Force
-		}
-		New-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\SystemAppData\Microsoft.549981C3F5F10_8wekyb3d8bbwe\CortanaStartupId" -Name State -PropertyType DWord -Value 1 -Force
-	}
-}
-#endregion UWP apps
 #region Microsoft Defender & Security
 # Turn on Microsoft Defender Exploit Guard network protection
 # Включить защиту сети в Microsoft Defender Exploit Guard
@@ -2433,6 +2314,125 @@ function ThreadPriority {
 	New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\services\mouclass\Parameters" -Name "MouseDataQueueSize" -PropertyType DWord -Value 20 -Force
 }
 #endregion Performance
+#region UWP apps
+<#
+	Uninstall UWP apps
+	A dialog box that enables the user to select packages to remove
+	App packages will not be installed for new users if "Uninstall for All Users" is checked
+	Add UWP apps packages names to the $UncheckedAppXPackages array list by retrieving their packages names using the following command:
+		(Get-AppxPackage -PackageTypeFilter Bundle -AllUsers).Name
+
+	Удалить UWP-приложения
+	Диалоговое окно, позволяющее пользователю отметить пакеты на удаление
+	Приложения не будут установлены для новых пользователе, если отмечено "Удалять для всех пользователей"
+	Добавьте имена пакетов UWP-приложений в массив $UncheckedAppXPackages, получив названия их пакетов с помощью команды:
+		(Get-AppxPackage -PackageTypeFilter Bundle -AllUsers).Name
+#>
+function UninstallUWPApps {
+	# UWP apps that won't be shown in the form
+	# UWP-приложения, которые не будут выводиться в форме
+	$ExcludedAppxPackages = @(
+
+		# Microsoft Store
+		"Microsoft.WindowsStore",
+		
+		# AMD Radeon UWP panel
+		# UWP-панель AMD Radeon
+		"AdvancedMicroDevicesInc*",
+
+		# NVIDIA Control Panel
+		# Панель управления NVidia
+		"NVIDIACorp.NVIDIAControlPanel",
+
+		# Realtek Audio Control
+		"RealtekSemiconductorCorp.RealtekAudioControl"
+		
+	)
+	
+	if (Get-AppxPackage -PackageTypeFilter Bundle -AllUsers | Where-Object -FilterScript { $_.Name -cnotmatch ($ExcludedAppxPackages -join "|") } | Remove-AppxPackage -AllUsers ) {
+		Write-Verbose -Message 'Removed UWP apps' -Verbose
+	}
+	else {
+		Write-Verbose -Message $Localization.NoData -Verbose
+	}
+}
+
+# Do not let UWP apps run in the background, except the followings... (current user only)
+# Не разрешать UWP-приложениям работать в фоновом режиме, кроме следующих... (только для текущего пользователя)
+function DisableBackgroundUWPApps {
+	New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications -Name GlobalUserDisabled -PropertyType DWord -Value 1 -Force
+	Get-ChildItem -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications | ForEach-Object -Process {
+		Remove-ItemProperty -Path $_.PsPath -Name * -Force
+	}
+
+	$ExcludedBackgroundApps = @(
+
+		# Windows Search
+		"Microsoft.Windows.Search",
+
+		# Windows Security
+		# Безопасность Windows
+		"Microsoft.Windows.SecHealthUI",
+
+		# Microsoft Store
+		"Microsoft.WindowsStore"
+	)
+	$OFS = "|"
+	Get-ChildItem -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications | Where-Object -FilterScript { $_.PSChildName -notmatch "^$($ExcludedBackgroundApps.ForEach({[regex]::Escape($_)}))" } | ForEach-Object -Process {
+		New-ItemProperty -Path $_.PsPath -Name Disabled -PropertyType DWord -Value 1 -Force
+		New-ItemProperty -Path $_.PsPath -Name DisabledByUser -PropertyType DWord -Value 1 -Force
+	}
+	$OFS = " "
+}
+
+# Disable the following Windows features
+# Отключить следующие компоненты Windows
+function DisableWindowsFeatures {
+	$WindowsOptionalFeatures = @(
+		# Media Features
+		# Компоненты работы с мультимедиа
+		"MediaPlayback",
+
+		# Work Folders Client
+		# Клиент рабочих папок
+		"WorkFolders-Client"
+	)
+	Disable-WindowsOptionalFeature -Online -FeatureName $WindowsOptionalFeatures -NoRestart
+}
+
+# Disable certain Feature On Demand v2 (FODv2) capabilities
+# Отключить определенные компоненты "Функции по требованию" (FODv2)
+function DisableWindowsCapabilities {
+	# The following FODv2 items will be shown, but their checkboxes would be clear
+	# Следующие дополнительные компоненты будут видны, но их чекбоксы не будут отмечены
+	$ExcludedCapabilities = @(
+		# The DirectX Database to configure and optimize apps when multiple Graphics Adapters are present
+		# База данных DirectX для настройки и оптимизации приложений при наличии нескольких графических адаптеров
+		"DirectX.Configuration.Database*",
+
+		# Language components
+		"Language.*"
+	)
+	
+	if (Get-WindowsCapability -Online | Where-Object -FilterScript { ($_.State -eq "Installed") -and ($_.Name -cnotmatch ($ExcludedCapabilities -join "|")) } | Remove-WindowsCapability -Online ) {
+		Write-Verbose -Message 'Removed Capabilities' -Verbose
+	}
+	else {
+		Write-Verbose -Message $Localization.NoData -Verbose
+	}
+}
+
+# Turn off Cortana autostarting
+# Удалить Кортана из автозагрузки
+function DisableCortanaAutostart {
+	if (Get-AppxPackage -Name Microsoft.549981C3F5F10) {
+		if (-not (Test-Path -Path "Registry::HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\SystemAppData\Microsoft.549981C3F5F10_8wekyb3d8bbwe\CortanaStartupId")) {
+			New-Item -Path "Registry::HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\SystemAppData\Microsoft.549981C3F5F10_8wekyb3d8bbwe\CortanaStartupId" -Force
+		}
+		New-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\SystemAppData\Microsoft.549981C3F5F10_8wekyb3d8bbwe\CortanaStartupId" -Name State -PropertyType DWord -Value 1 -Force
+	}
+}
+#endregion UWP apps
 #region System
 # Uninstall OneDrive
 # Удалить OneDrive
