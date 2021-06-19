76c577f475ba64cfe39d25c7521d2da842576f71
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-06-19 09:44:21 +0100

    Additional commit

--- Fresh/Fresh.psm1
+++ Fresh/Fresh.psm1
@@ -1481,6 +1481,113 @@ function DisableScheduledTasks {
 	schtasks /Change /TN "Microsoft\Windows\Time Synchronization\SynchronizeTime" /Disable
 	schtasks /Change /TN "Microsoft\Windows\Windows Error Reporting\QueueReporting" /Disable
 	schtasks /Change /TN "Microsoft\Windows\WindowsUpdate\Automatic App Update" /Disable
+	schtasks /DELETE /TN "Adobe Flash Player PPAPI Notifier" /f
+	schtasks /DELETE /TN "Adobe Flash Player Updater" /f
+	schtasks /DELETE /TN "AMDLinkUpdate" /f
+	schtasks /DELETE /TN "Driver Easy Scheduled Scan" /f
+	schtasks /DELETE /TN "GPU Tweak II" /f
+	schtasks /DELETE /TN "klcp_update" /f
+	schtasks /DELETE /TN "ModifyLinkUpdate" /f
+	schtasks /DELETE /TN "Repairing Yandex Browser update service" /f
+	schtasks /DELETE /TN "StartDVR" /f
+	schtasks /DELETE /TN "StartCN" /f
+	schtasks /DELETE /TN "System update for Yandex Browser" /f
+	schtasks /DELETE /TN "Update for Yandex Browser" /f
+	schtasks /Change /TN "CreateExplorerShellUnelevatedTask" /Enable
+	schtasks /Change /TN "Microsoft\Windows\.NET Framework\.NET Framework NGEN v4.0.30319" /Disable
+	schtasks /Change /TN "Microsoft\Windows\.NET Framework\.NET Framework NGEN v4.0.30319 64" /Disable
+	schtasks /Change /TN "Microsoft\Windows\.NET Framework\.NET Framework NGEN v4.0.30319 64 Critical" /Disable
+	schtasks /Change /TN "Microsoft\Windows\.NET Framework\.NET Framework NGEN v4.0.30319 Critical" /Disable
+	schtasks /Change /TN "Microsoft\Windows\ApplicationData\appuriverifierdaily" /Disable
+	schtasks /Change /TN "Microsoft\Windows\ApplicationData\appuriverifierinstall" /Disable
+	schtasks /Change /TN "Microsoft\Windows\ApplicationData\CleanupTemporaryState" /Disable
+	schtasks /Change /TN "Microsoft\Windows\ApplicationData\DsSvcCleanup" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Application Experience\ProgramDataUpdater" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Application Experience\StartupAppTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\AppxDeploymentClient\Pre-staged app cleanup" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Autochk\Proxy" /Disable
+	schtasks /Change /TN "Microsoft\Windows\BrokerInfrastructure\BgTaskRegistrationMaintenanceTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\CloudExperienceHost\CreateObjectTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Device Information\Device" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Defrag\ScheduledDefrag" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Diagnosis\Scheduled" /Disable
+	schtasks /Change /TN "Microsoft\Windows\DiskCleanup\SilentCleanup" /Disable
+	schtasks /Change /TN "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /Disable
+	schtasks /Change /TN "Microsoft\Windows\DiskFootprint\Diagnostics" /Disable
+	schtasks /Change /TN "Microsoft\Windows\DiskFootprint\StorageSense" /Disable
+	schtasks /Change /TN "Microsoft\Windows\DUSM\dusmtask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\EnterpriseMgmt\MDMMaintenenceTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Feedback\Siuf\DmClient" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Feedback\Siuf\DmClientOnScenarioDownload" /Disable
+	schtasks /Change /TN "Microsoft\Windows\FileHistory\File History (maintenance mode)" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Flighting\OneSettings\RefreshCache" /Disable
+	schtasks /Change /TN "Microsoft\Windows\HelloFace\FODCleanupTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\InstallService\ScanForUpdates" /Disable
+	schtasks /Change /TN "Microsoft\Windows\InstallService\ScanForUpdatesAsUser" /Disable
+	schtasks /Change /TN "Microsoft\Windows\InstallService\WakeUpAndContinueUpdates" /Disable
+	schtasks /Change /TN "Microsoft\Windows\InstallService\WakeUpAndScanForUpdates" /Disable
+	schtasks /Change /TN "Microsoft\Windows\InstallService\SmartRetry" /Disable
+	schtasks /Change /TN "Microsoft\Windows\LanguageComponentsInstaller\Installation" /Disable
+	schtasks /Change /TN "Microsoft\Windows\LanguageComponentsInstaller\ReconcileLanguageResources" /Disable
+	schtasks /Change /TN "Microsoft\Windows\LanguageComponentsInstaller\Uninstallation" /Disable
+	schtasks /Change /TN "Microsoft\Windows\License Manager\TempSignedLicenseExchange" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Location\Notifications" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Location\WindowsActionDialog" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Management\Provisioning\Cellular" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Management\Provisioning\Logon" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Maintenance\WinSAT" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Maps\MapsToastTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Maps\MapsUpdateTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Mobile Broadband Accounts\MNO Metadata Parser" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Multimedia\SystemSoundsService" /Disable
+	schtasks /Change /TN "Microsoft\Windows\NlaSvc\WiFiTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\NetTrace\GatherNetworkInfo" /Disable
+	schtasks /Change /TN "Microsoft\Windows\PI\Sqm-Tasks" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Power Efficiency Diagnostics\AnalyzeSystem" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Printing\EduPrintProv" /Disable
+	schtasks /Change /TN "Microsoft\Windows\PushToInstall\Registration" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Ras\MobilityManager" /Disable
+	schtasks /Change /TN "Microsoft\Windows\RecoveryEnvironment\VerifyWinRE" /Disable
+	schtasks /Change /TN "Microsoft\Windows\RemoteAssistance\RemoteAssistanceTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\RetailDemo\CleanupOfflineContent" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Servicing\StartComponentCleanup" /Disable
+	schtasks /Change /TN "Microsoft\Windows\SettingSync\BackgroundUploadTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\SettingSync\BackupTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\SettingSync\NetworkStateChangeTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Shell\CreateObjectTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Setup\SetupCleanupTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\SpacePort\SpaceAgentTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\SpacePort\SpaceManagerTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Speech\HeadsetButtonPress" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Speech\SpeechModelDownloadTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Storage Tiers Management\Storage Tiers Management Initialization" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Subscription\EnableLicenseAcquisition" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Subscription\LicenseAcquisition" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Sysmain\ResPriStaticDbSync" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Sysmain\WsSwapAssessmentTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Task Manager\Interactive" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Time Synchronization\ForceSynchronizeTime" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Time Synchronization\SynchronizeTime" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Time Zone\SynchronizeTimeZone" /Disable
+	schtasks /Change /TN "Microsoft\Windows\TPM\Tpm-HASCertRetr" /Disable
+	schtasks /Change /TN "Microsoft\Windows\TPM\Tpm-Maintenance" /Disable
+	schtasks /Change /TN "Microsoft\Windows\UPnP\UPnPHostConfig" /Disable
+	schtasks /Change /TN "Microsoft\Windows\USB\Usb-Notifications" /Disable
+	schtasks /Change /TN "Microsoft\Windows\User Profile Service\HiveUploadTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\WCM\WiFiTask" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Windows Filtering Platform\BfeOnServiceStartTypeChange" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Windows Media Sharing\UpdateLibrary" /Disable
+	schtasks /Change /TN "Microsoft\Windows\WindowsUpdate\Scheduled Start" /Disable
+	schtasks /Change /TN "Microsoft\Windows\WlanSvc\CDSSync" /Disable
+	schtasks /Change /TN "Microsoft\Windows\WOF\WIM-Hash-Management" /Disable
+	schtasks /Change /TN "Microsoft\Windows\WOF\WIM-Hash-Validation" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Work Folders\Work Folders Logon Synchronization" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Work Folders\Work Folders Maintenance Work" /Disable
+	schtasks /Change /TN "Microsoft\Windows\Workplace Join\Automatic-Device-Join" /Disable
+	schtasks /Change /TN "Microsoft\Windows\WwanSvc\NotificationTask" /Disable
 	REG DELETE HKSU\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /f
 	REG DELETE HKSU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f
 	REG DELETE HKSU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies /f
@@ -1848,11 +1955,9 @@ function UninstallUWPApps {
 		"Microsoft.WindowsStore",
 		
 		# AMD Radeon UWP panel
-		# UWP-панель AMD Radeon
 		"AdvancedMicroDevicesInc*",
 
 		# NVIDIA Control Panel
-		# Панель управления NVidia
 		"NVIDIACorp.NVIDIAControlPanel",
 
 		# Realtek Audio Control
@@ -1882,7 +1987,6 @@ function DisableBackgroundUWPApps {
 		"Microsoft.Windows.Search",
 
 		# Windows Security
-		# Безопасность Windows
 		"Microsoft.Windows.SecHealthUI",
 
 		# Microsoft Store
