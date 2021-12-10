bed55b21d5e703374bd166218fe361c07a8a1e5e
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-12-10 15:20:30 +0300

    edit Fresh/Fresh.psm1

--- Fresh/Fresh.psm1
+++ Fresh/Fresh.psm1
@@ -3283,6 +3283,8 @@ function DebloatMicrosoftServices {
 	Set-Service RmSvc -StartupType Disabled -ErrorAction SilentlyContinue
 	Stop-Service "seclogon" -Force -WarningAction SilentlyContinue
 	Set-Service seclogon -StartupType Disabled -ErrorAction SilentlyContinue
+	Stop-Service "sethc" -Force -WarningAction SilentlyContinue
+	Set-Service sethc -StartupType Disabled -ErrorAction SilentlyContinue
 	Stop-Service "StorSvc" -Force -WarningAction SilentlyContinue
 	Set-Service StorSvc -StartupType Disabled -ErrorAction SilentlyContinue
 	Stop-Service "TrkWks" -Force -WarningAction SilentlyContinue
