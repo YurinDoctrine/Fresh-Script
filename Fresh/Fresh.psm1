c7be32af6143827aba3ad5f3977624f1dbb118e1
Yurin Doctrine <doctriney@protonmail.com>
2023-05-19 12:30:15 +0300

    edit Fresh/Fresh.psm1

--- Fresh/Fresh.psm1
+++ Fresh/Fresh.psm1
@@ -2147,9 +2147,9 @@ function BestPriorityForeground {
     auditpol /set /category:"System" /success:disable
     auditpol /set /category:"System" /failure:disable
 
-    Set-MpPreference -ScanOnlyIfIdleEnabled 0
     Set-MpPreference -ScanAvgCPULoadFactor 1
     Set-MpPreference -EnableLowCpuPriority $True
+    Set-MpPreference -ScanOnlyIfIdleEnabled $True
 
     Set-SmbServerConfiguration -ServerHidden $False -AnnounceServer $False -Force
     Set-SmbServerConfiguration -EnableLeasing $false -Force
