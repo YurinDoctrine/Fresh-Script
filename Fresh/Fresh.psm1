50d294c6fc41cb1497c2571fa15fbff30d00b659
Yurin Doctrine <doctriney@protonmail.com>
2023-05-19 12:32:37 +0300

    edit Fresh/Fresh.psm1

--- Fresh/Fresh.psm1
+++ Fresh/Fresh.psm1
@@ -2147,7 +2147,7 @@ function BestPriorityForeground {
     auditpol /set /category:"System" /success:disable
     auditpol /set /category:"System" /failure:disable
 
-    Set-MpPreference -ScanAvgCPULoadFactor 1
+    Set-MpPreference -ScanAvgCPULoadFactor 5
     Set-MpPreference -EnableLowCpuPriority $True
     Set-MpPreference -ScanOnlyIfIdleEnabled $True
 
