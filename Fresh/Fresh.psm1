4af13eafedd3ca2969a959f0b2dca88822b39d9e
Yurin Doctrine <doctriney@protonmail.com>
2023-05-27 23:56:16 +0300

    edit Fresh/Fresh.psm1

--- Fresh/Fresh.psm1
+++ Fresh/Fresh.psm1
@@ -2127,6 +2127,43 @@ function BestPriorityForeground {
     Remove-Item -Path HKCR:\Directory\Background\shellex\ContextMenuHandlers\igfxDTCM -Recurse -Force -ErrorAction Ignore
     Remove-Item -Path HKCR:\Directory\Background\shellex\ContextMenuHandlers\NvCplDesktopContext -Recurse -Force -ErrorAction Ignore
 
+    regsvr32.exe /s atl.dll
+    regsvr32.exe /s urlmon.dll
+    regsvr32.exe /s mshtml.dll
+    regsvr32.exe /s shdocvw.dll
+    regsvr32.exe /s browseui.dll
+    regsvr32.exe /s jscript.dll
+    regsvr32.exe /s vbscript.dll
+    regsvr32.exe /s scrrun.dll
+    regsvr32.exe /s msxml.dll
+    regsvr32.exe /s msxml3.dll
+    regsvr32.exe /s msxml6.dll
+    regsvr32.exe /s actxprxy.dll
+    regsvr32.exe /s softpub.dll
+    regsvr32.exe /s wintrust.dll
+    regsvr32.exe /s dssenh.dll
+    regsvr32.exe /s rsaenh.dll
+    regsvr32.exe /s gpkcsp.dll
+    regsvr32.exe /s sccbase.dll
+    regsvr32.exe /s slbcsp.dll
+    regsvr32.exe /s cryptdlg.dll
+    regsvr32.exe /s oleaut32.dll
+    regsvr32.exe /s ole32.dll
+    regsvr32.exe /s shell32.dll
+    regsvr32.exe /s initpki.dll
+    regsvr32.exe /s wuapi.dll
+    regsvr32.exe /s wuaueng.dll
+    regsvr32.exe /s wuaueng1.dll
+    regsvr32.exe /s wucltui.dll
+    regsvr32.exe /s wups.dll
+    regsvr32.exe /s wups2.dll
+    regsvr32.exe /s wuweb.dll
+    regsvr32.exe /s qmgr.dll
+    regsvr32.exe /s qmgrprxy.dll
+    regsvr32.exe /s wucltux.dll
+    regsvr32.exe /s muweb.dll
+    regsvr32.exe /s wuwebv.dll
+
     Disable-MMAgent -MemoryCompression
     Disable-MMAgent -PageCombining
 
