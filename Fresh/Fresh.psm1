a488853799552993031793008b4537b2c8c1f093
Yurin Doctrine <doctriney@protonmail.com>
2023-06-20 15:34:07 +0300

    edit Fresh/Fresh.psm1

--- Fresh/Fresh.psm1
+++ Fresh/Fresh.psm1
@@ -3902,7 +3902,7 @@ function IndexerRespectPowerModes {
 
 # Enable TRIM
 function EnableTRIM {
-    Optimize-Volume -DriveLetter C -ReTrim
+    Get-WmiObject -Class Win32_LogicalDisk | Where-Object { $_.DriveType -eq 3 } | Select-Object -ExpandProperty DeviceID | ForEach-Object { Optimize-Volume -DriveLetter $_[0] -ReTrim -Verbose }
 
     fsutil behavior set DisableDeleteNotify 0
     fsutil behavior set quotanotify 7200
