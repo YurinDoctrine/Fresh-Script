989acc36fa3eeb0a05da851c218a1f4b6ab4fb9a
Yurin Doctrine <doctriney@protonmail.com>
2024-04-02 21:12:11 +0300

    edit Fresh/Fresh.psm1

--- Fresh/Fresh.psm1
+++ Fresh/Fresh.psm1
@@ -2356,6 +2356,10 @@ function BestPriorityForeground {
     auditpol /set /category:"Detailed Tracking" /failure:disable
     auditpol /set /category:"System" /success:disable
     auditpol /set /category:"System" /failure:disable
+    auditpol /set /subcategory:"System Integrity" /success:disable
+    auditpol /set /subcategory:"Other System Events" /success:disable
+    auditpol /set /subcategory:"Security System Extension" /success:disable
+    auditpol /set /subcategory:"Security State Change" /success:disable
 
     wevtutil sl Application /ms:0
     wevtutil sl Setup /ms:0
